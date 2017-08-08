677745022fe2dd53b4f5dc368d4d0d332aed2421
Jakub <jakewski@gmail.com>
2017-08-08 16:27:57 -0500

    kinda started

--- client/socket.js
+++ client/socket.js
@@ -1,9 +1,27 @@
 import io from 'socket.io-client';
-
+import { addPlayerThunk } from './store'
+import store from './store'
 const socket = io(window.location.origin);
 
-socket.on('connect', function () {
+socket.on('connect', () => {
   console.log('Connected!');
 });
+
+socket.on('addPlayer', socketId => {
+  console.log('hitit')
+  store.dispatch(addPlayerThunk(socketId));
+})
+
+// let players = [];
+// socket.on('grabId', function (socketId) {
+//   players.push(socketId);
+// })
+
+
+// socket.on('dosomethin', function () {
+//   console.log('was submitted')
+// })
+
+// export const emitSubmitAnswer = answer => socket.emit('submitAnswer', { answer } );
 
 export default socket
--- server/socket/index.js
+++ server/socket/index.js
@@ -1,6 +1,12 @@
 module.exports = (io) => {
   io.on('connection', (socket) => {
     console.log(`A socket connection to the server has been made: ${socket.id}`)
+    socket.emit('addPlayer', socket.id);
+
+    // socket.on('submitAnswer', payload => {
+    //   console.log(payload.answer);
+    // })
+
 
     socket.on('disconnect', () => {
       console.log(`Connection ${socket.id} has left the building`)
