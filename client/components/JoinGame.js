bd264a8537b5e0c1f2b35046ff3a5df10b27da95
Anne Marie King <amking23@gmail.com>
2017-08-09 10:28:17 -0500

    join game linked up

--- client/components/JoinGame.js
+++ client/components/JoinGame.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { NavLink } from 'react-router-dom'
 
 class JoinGame extends Component {
   constructor(){
@@ -13,6 +14,7 @@ class JoinGame extends Component {
     return (
       <div>
         <h1>Join Game</h1>
+        <NavLink to="/room"><button type="button" className="btn btn-success btn-lg">Enter Game</button></NavLink>
       </div>
     )
   }
