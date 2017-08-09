d586fdd87aa4ca38e12e1eb3210a399723ed8ee1
Anne Marie King <amking23@gmail.com>
2017-08-09 12:02:18 -0500

    outline for room and forms

--- client/components/CreateGame.js
+++ client/components/CreateGame.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { NavLink } from 'react-router-dom'
 
 class CreateGame extends Component {
   constructor(){
@@ -12,7 +13,56 @@ class CreateGame extends Component {
   render() { 
     return (
       <div>
-        <h1>In create a game</h1>
+        <h1>Create a New Game</h1>
+          <form className="form-group">
+
+            <div className="col"></div>
+            <div className="col">
+              <label className="sr-only" htmlFor="inlineFormInput">Enter Name</label>
+              <input type="text" className="form-control mb-2 mr-sm-2 mb-sm-0" id="inlineFormInput" placeholder="Enter Name" />
+            </div>
+            <div className="col"></div>
+
+            <br />
+            <br />
+
+            <label className="mr-sm-2" htmlFor="inlineFormCustomSelect">Number of Players</label>
+            <select className="custom-select mb-2 mr-sm-2 mb-sm-0" id="inlineFormCustomSelect">
+              <option value="3">Three</option>
+              <option value="4">Four</option>
+              <option value="5">Five</option>
+              <option value="6">Six</option>
+              <option value="7">Seven</option>
+              <option value="8">Eight</option>
+              <option value="9">Nine</option>
+              <option value="10">Ten</option>
+            </select> 
+
+            <br />
+            <br />
+
+            <div className="form-check noMargin">
+              <label className="form-check-label">Select Categories:
+                <br />
+                <input className="form-check-input" type="checkbox" value="" />
+                Option one is this and that&mdash;be sure to include why it's great
+              </label>
+            </div>
+
+            <br />
+
+            <div className="form-check noMargin">
+              <label className="form-check-label">
+                <input className="form-check-input" type="checkbox" value="" />
+                Option one is this and that&mdash;be sure to include why it's great
+              </label>
+            </div>
+
+            <br />
+            <br />
+
+            <NavLink to="/room"><button type="submit" className="btn btn-success">Create</button></NavLink>
+          </form>
       </div>
     )
   }
--- client/components/JoinGame.js
+++ client/components/JoinGame.js
@@ -14,7 +14,30 @@ class JoinGame extends Component {
     return (
       <div>
         <h1>Join Game</h1>
-        <NavLink to="/room"><button type="button" className="btn btn-success btn-lg">Enter Game</button></NavLink>
+
+        <form className="form-group">
+
+            <div className="col"></div>
+            <div className="col">
+              <label className="sr-only" htmlFor="inlineFormInput">Enter Name</label>
+              <input type="text" className="form-control mb-2 mr-sm-2 mb-sm-0" id="inlineFormInput" placeholder="Enter Name" />
+            </div>
+            <div className="col"></div>
+
+            <br />
+
+            <div className="col"></div>
+            <div className="col">
+              <label className="sr-only" htmlFor="inlineFormInput">Enter Room Code</label>
+              <input type="text" className="form-control mb-2 mr-sm-2 mb-sm-0" id="inlineFormInput" placeholder="Enter Room Code" />
+            </div>
+            <div className="col"></div>
+
+            <br />
+            <br />
+
+            <NavLink to="/room"><button type="submit" className="btn btn-success">Join</button></NavLink>
+          </form>
       </div>
     )
   }
