103573eb9aaebc5ad5af5722112eb09e2d92d34a
Anne Marie King <amking23@gmail.com>
2017-08-10 14:49:16 -0500

    hey whats your name validation

--- client/components/EnterName.js
+++ client/components/EnterName.js
@@ -6,10 +6,37 @@ import { CSSTransitionGroup } from 'react-transition-group';
 class EnterName extends Component {
   constructor(){
     super();
+    this.state = {
+      player: {name: '', dirty: false}
+    }
   }
 
   componentDidMount() {
 
+  }
+
+  updateField(e) {
+    this.setState({
+      player: {name: e.target.value, dirty: true}
+    })
+    console.log('player name: ', this.state.player.name);
+  }
+
+  formSubmit(e) {
+    e.preventDefault();
+    this.setState(initialState);
+  }
+
+  inputIsEmpty(){
+    return this.state.player.name.length === 0;
+  }
+
+  inputWithinLimit(){
+    return this.state.player.name.length <= 16
+  }
+
+  inputIsValid(){
+    return !this.inputIsEmpty() && this.inputWithinLimit()
   }
 
   render() {
@@ -17,12 +44,21 @@ class EnterName extends Component {
       <div className="container-fluid">
         <div className="row">
           <CSSTransitionGroup transitionName="example" transitionAppear={true} transitionAppearTimeout={2000} transitionEnterTimeout={0} transitionLeaveTimeout={0}>
-            <form key="transition" className="form-inline">
+            <form key="transition" className="form-inline" onSubmit={(e) => this.formSubmit(e)}>
               <h1 className="whatsYourName">Hello, what is your name?</h1>
               <br />
               <label className="sr-only" htmlFor="inlineFormInput">Name</label>
-              <input type="text" className="form-control mb-2 mr-sm-2 mb-sm-0" id="inlineFormInput" placeholder="Jane Doe" />
-              <NavLink to="/home"><button type="submit" className="btn btn-success marginLeft">Enter</button></NavLink>
+              <input value={this.state.playerName} onChange={(e) => this.updateField(e)} type="text" className="form-control mb-2 mr-sm-2 mb-sm-0" id="inlineFormInput" placeholder="Jane Doe" />
+              <NavLink to="/home"><button type="submit" className="btn btn-success marginLeft" disabled={!this.inputIsValid()}>Enter</button></NavLink>
+              <br />
+              {
+                  this.inputIsEmpty() && this.state.player.dirty ?
+                  <span className="alert alert-danger validationSpan">You must enter a name</span> :
+                  <span className="alert alert-danger emptySpan"></span> }
+                  {!this.inputWithinLimit() && this.state.player.dirty ?
+                  <span className="alert alert-danger validationSpan">Name too long</span> :
+                  null 
+              }
             </form>
           </CSSTransitionGroup>
       </div>
