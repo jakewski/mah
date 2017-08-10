57a074e1fdbe9a8e141e55f26880257a1dd0f559
Jakub <jakewski@gmail.com>
2017-08-09 20:06:53 -0500

    working create game

--- client/components/GameRoom.js
+++ client/components/GameRoom.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-
+import socket from '../socket'
 class GameRoom extends Component {
   constructor(){
     super();
