6f1fbe87855a9b5c4a5a98935b548fa5ba84f74b
Anne Marie King <amking23@gmail.com>
2017-08-16 16:25:28 -0500

    host can now see game players

--- client/components/Pregame.js
+++ client/components/Pregame.js
@@ -26,22 +26,22 @@ class Pregame extends Component {
         {(this.props.players.players[0].sessionId === this.props.players.player.sessionId) ?
           <button onClick={this.handleClick} className="btn btn-success">
             Start Da Game Bro
-                </button>
+          </button>
           :
           <div>
             <h1>Please wait until host chooses to start the game</h1>
-            <h3>Game Players: </h3>
-            <h5>Host: {this.props.players.players[0].name}</h5>
-            {
-              this.props.players.players.map((player, index) => {
-                if (!(index === 0)) {
-                  return (
-                    <div key={index} >Player {index + 1}: {player.name}</div>
-                  )
-                }
-              })
-            }
           </div>
+        }
+        <h3>Game Players: </h3>
+        <h5>Host: {this.props.players.players[0].name}</h5>
+        {
+          this.props.players.players.map((player, index) => {
+            if (!(index === 0)) {
+              return (
+                <div key={index} >Player {index + 1}: {player.name}</div>
+              )
+            }
+          })
         }
       </div>
 
