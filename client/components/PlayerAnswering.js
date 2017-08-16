436e30ee915fe58606662ec01b1e5a6847856b4a
Madelyn Romzek <mrromzek@umich.edu>
2017-08-16 12:35:45 -0500

    canvas text can be dragged and font size can be changed

--- client/components/PlayerAnswering.js
+++ client/components/PlayerAnswering.js
@@ -8,24 +8,56 @@ export default class PlayerAnswering extends React.Component {
 
         this.state = {
             topText: '',
+            topXcoord: 10,
+            topYcoord: 10,
             bottomText: '',
+            bottomXcoord: 10,
+            bottomYcoord: 0,
+            topFontSize: 40,
+            bottomFontSize: 40,
             memeImg: null, //DO NOT CHANGE THIS -- React-konva relies on the null keyword
-            memeSize: {}
         };
 
         this.topTxtChange = this.topTxtChange.bind(this);
         this.bottomTxtChange = this.bottomTxtChange.bind(this);
+        this.dragTop = this.dragTop.bind(this);
+        this.drgaBottom = this.dragBottom.bind(this);
+        this.onMemeSubmit = this.onMemeSubmit.bind(this);
+        this.onTopPlusClick = this.onTopPlusClick.bind(this);
+        this.onTopMinusClick = this.onTopMinusClick.bind(this);
+        this.onBottomPlusClick = this.onBottomPlusClick.bind(this);
+        this.onBottomMinusClick = this.onBottomMinusClick.bind(this);
     }
 
     componentDidMount() {
         const image = new window.Image();
         image.src = 'https://imgflip.com/s/meme/Futurama-Fry.jpg';
         image.onload = () => {
-            this.setState({
-                memeImg: image
-            });
-
+          this.setState({
+            memeImg: image
+          })
+        this.setState({bottomYcoord: this.state.memeImg.height - 50})
         };
+    }
+
+    onTopPlusClick = (e) => {
+      e.preventDefault();
+      this.setState({topFontSize: this.state.topFontSize + 5});
+    }
+
+    onTopMinusClick = (e) => {
+      e.preventDefault();
+      this.setState({topFontSize: this.state.topFontSize - 5});
+    }
+
+    onBottomPlusClick = (e) => {
+      e.preventDefault();
+      this.setState({bottomFontSize: this.state.bottomFontSize + 5});
+    }
+
+    onBottomMinusClick = (e) => {
+      e.preventDefault();
+      this.setState({bottomFontSize: this.state.bottomFontSize - 5});
     }
 
     topTxtChange = (e) => {
@@ -36,16 +68,41 @@ export default class PlayerAnswering extends React.Component {
       this.setState({bottomText: e.target.value})
     }
 
+    dragTop = (e) => {
+      this.setState({
+        topXcoord: e.target.attrs.x,
+        topYcoord: e.target.attrs.y
+      });
+    }
+
+    dragBottom = (e) => {
+      this.setState({
+        bottomXcoord: e.target.attrs.x,
+        bottomYcoord: e.target.attrs.y
+      })
+    }
+
+    onMemeSubmit = () => {
+      //send almost entire state to db
+      //we will recreate the memes via konva canvas on the slider
+    }
+
     render() {
-      console.log(this.state)
         return (
             <div>
               <div className="gameAnswerFlex">
                 <form>
                   <div className="form-group col-md-9 col-xs-12 col-lg-6" >
-                    <input placeholder="top text" className="form-control formPlaceholder" id="formInputTop" onChange={this.topTxtChange}/>
-                    <input placeholder="bottom text" className="form-control formPlaceholder" id="formInputBottom" onChange={this.bottomTxtChange}/>
-                    <button type="submit" className="btn btn-success">Submit</button>
+                    <input placeholder="top text" className="form-control formPlaceholder memeInput" id="formInputTop" onChange={this.topTxtChange}/>
+                    <button onClick={this.onTopPlusClick}>+</button>
+                    <button onClick={this.onTopMinusClick}>-</button>
+
+                    <input placeholder="bottom text" className="form-control formPlaceholder memeInput" id="formInputBottom" onChange={this.bottomTxtChange}/>
+                    <button onClick={this.onBottomPlusClick}>+</button>
+                    <button onClick={this.onBottomMinusClick}>-</button>
+                    <br />
+                    <br />
+                    <button type="submit" className="btn btn-success center">Submit</button>
                   </div>
                 </form>
 
@@ -55,8 +112,8 @@ export default class PlayerAnswering extends React.Component {
                       <Image image={this.state.memeImg} />
                     </Layer>
                     <Layer>
-                      <Text x={10} y={10} fontSize={40} fontFamily='Impact' fill='white' allign='center' text={this.state.topText}/>
-                      <Text x={10} y={this.state.memeImg.height - 50} fontSize={40} fontFamily='Impact' fill='white' allign='center' text={this.state.bottomText}/>
+                      <Text align="center" x={this.state.topXcoord} y={this.state.topYcoord} fontSize={this.state.topFontSize} fontFamily='Impact' fill='white' wrap='char' draggable={true} shadowColor='black' text={this.state.topText} onDragEnd={this.dragTop} />
+                      <Text x={this.state.bottomXcoord} y={this.state.bottomYcoord} fontSize={this.state.bottomFontSize} fontFamily='Impact' fill='white' draggable={true} shadowColor='black' text={this.state.bottomText} onDragEnd={this.dragBottom}/>
                     </Layer>
                   </Stage>
                 : <div />}
