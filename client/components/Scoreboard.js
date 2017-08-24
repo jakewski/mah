e8c7d277409b2b370e703f767311bc899a1d4c52
Jakub <jakewski@gmail.com>
2017-08-24 10:42:57 -0400

    done

--- client/components/Scoreboard.js
+++ client/components/Scoreboard.js
@@ -36,40 +36,6 @@ class Scoreboard extends React.Component {
     return (
       <div>
         <div className="row">
-          <div className="playerScoreFlexBox">
-            {this.props.players.map((player, index) => {
-              return (
-                <div key={index}>
-                  {
-                    this.props.judge.id === player.id ?
-                      this.props.allAnswersSubmitted && !this.state.roundUnjudged ?
-                        <div>
-                          <div className="scoreText blue name" key={index}>{player.name}: {player.score} </div>
-                          <div className="loadingBlue right load"></div>
-                        </div>
-                      :
-                        <div>
-                          <div className="scoreText blue" key={index}>{player.name}: {player.score} ★</div>
-                        </div>
-                    : (this.props.submittedAnswers && Object.keys(this.props.submittedAnswers).includes(player.id)) || this.state.timeout ?
-                        //if answer submitted OR timeout
-                        //if submitted
-                        Object.keys(this.props.submittedAnswers).includes(player.id) ?
-                            <div className="scoreText green" key={index}>{player.name}: {player.score} ✓</div>
-                          : //if timeout
-                            <div>
-                              <div className="scoreText grey name" key={index}>{player.name}: {player.score} X</div>
-                            </div>
-                        : //waiting....
-                          <div>
-                            <div className="scoreText red name" key={index}>{player.name}: {player.score} </div>
-                            <div className="loadingRed right load"></div>
-                          </div>
-                  }
-                </div>
-              )
-            })}
-          </div>
           <ScoreboardHeader turnNumber={this.props.turnNumber} category={this.props.category} currentTimer={this.props.currentTimer}/>
         </div>
       </div>
